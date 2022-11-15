59037c219da94ce5372ff108c682b30bf0ad0163
Abdel <LENOVO@DESKTOP-JMSGIU7>
2022-11-15 10:42:09 +0100

    My first commit

---
+++ Debug/Middlewares/Third_Party/Freertos_Plus/FreeRTOS_TCP_IP.d
@@ -0,0 +1,101 @@
+Middlewares/Third_Party/Freertos_Plus/FreeRTOS_TCP_IP.o: \
+ ../Middlewares/Third_Party/Freertos_Plus/FreeRTOS_TCP_IP.c \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h \
+ ../Core/Inc/FreeRTOSConfig.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM7/r0p1/portmacro.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/task.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/list.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/queue.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/task.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/semphr.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/queue.h \
+ C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/FreeRTOS_IP.h \
+ ../Core/Inc/FreeRTOSIPConfig.h \
+ C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/FreeRTOSIPConfigDefaults.h \
+ C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/FreeRTOS_errno_TCP.h \
+ C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/IPTraceMacroDefaults.h \
+ C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/portable/Compiler/GCC/pack_struct_start.h \
+ C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/portable/Compiler/GCC/pack_struct_end.h \
+ C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/FreeRTOS_Sockets.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/event_groups.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/timers.h \
+ C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/FreeRTOS_IP_Private.h \
+ C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/FreeRTOS_Sockets.h \
+ C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/FreeRTOS_Stream_Buffer.h \
+ C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/FreeRTOS_TCP_WIN.h \
+ C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/FreeRTOS_TCP_IP.h \
+ C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/FreeRTOS_UDP_IP.h \
+ C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/FreeRTOS_DHCP.h \
+ C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/NetworkInterface.h \
+ C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/NetworkBufferManagement.h \
+ C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/FreeRTOS_ARP.h
+
+../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h:
+
+../Core/Inc/FreeRTOSConfig.h:
+
+../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h:
+
+../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h:
+
+../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h:
+
+../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM7/r0p1/portmacro.h:
+
+../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h:
+
+../Middlewares/Third_Party/FreeRTOS/Source/include/task.h:
+
+../Middlewares/Third_Party/FreeRTOS/Source/include/list.h:
+
+../Middlewares/Third_Party/FreeRTOS/Source/include/queue.h:
+
+../Middlewares/Third_Party/FreeRTOS/Source/include/task.h:
+
+../Middlewares/Third_Party/FreeRTOS/Source/include/semphr.h:
+
+../Middlewares/Third_Party/FreeRTOS/Source/include/queue.h:
+
+C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/FreeRTOS_IP.h:
+
+../Core/Inc/FreeRTOSIPConfig.h:
+
+C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/FreeRTOSIPConfigDefaults.h:
+
+C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/FreeRTOS_errno_TCP.h:
+
+C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/IPTraceMacroDefaults.h:
+
+C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/portable/Compiler/GCC/pack_struct_start.h:
+
+C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/portable/Compiler/GCC/pack_struct_end.h:
+
+C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/FreeRTOS_Sockets.h:
+
+../Middlewares/Third_Party/FreeRTOS/Source/include/event_groups.h:
+
+../Middlewares/Third_Party/FreeRTOS/Source/include/timers.h:
+
+C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/FreeRTOS_IP_Private.h:
+
+C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/FreeRTOS_Sockets.h:
+
+C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/FreeRTOS_Stream_Buffer.h:
+
+C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/FreeRTOS_TCP_WIN.h:
+
+C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/FreeRTOS_TCP_IP.h:
+
+C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/FreeRTOS_UDP_IP.h:
+
+C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/FreeRTOS_DHCP.h:
+
+C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/NetworkInterface.h:
+
+C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/NetworkBufferManagement.h:
+
+C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/FreeRTOS_ARP.h:
---
+++ Debug/Middlewares/Third_Party/Freertos_Plus/portable/NetworkInterface/STM32Fxx/NetworkInterface.d
@@ -0,0 +1,206 @@
+Middlewares/Third_Party/Freertos_Plus/portable/NetworkInterface/STM32Fxx/NetworkInterface.o: \
+ ../Middlewares/Third_Party/Freertos_Plus/portable/NetworkInterface/STM32Fxx/NetworkInterface.c \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h \
+ ../Core/Inc/FreeRTOSConfig.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM7/r0p1/portmacro.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/task.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/list.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/queue.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/task.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/semphr.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/queue.h \
+ C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/FreeRTOS_IP.h \
+ ../Core/Inc/FreeRTOSIPConfig.h \
+ C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/FreeRTOSIPConfigDefaults.h \
+ C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/FreeRTOS_errno_TCP.h \
+ C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/IPTraceMacroDefaults.h \
+ C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/portable/Compiler/GCC/pack_struct_start.h \
+ C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/portable/Compiler/GCC/pack_struct_end.h \
+ C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/FreeRTOS_Sockets.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/event_groups.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/timers.h \
+ C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/FreeRTOS_IP_Private.h \
+ C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/FreeRTOS_Sockets.h \
+ C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/FreeRTOS_Stream_Buffer.h \
+ C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/FreeRTOS_TCP_WIN.h \
+ C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/FreeRTOS_TCP_IP.h \
+ C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/FreeRTOS_DNS.h \
+ C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/FreeRTOS_ARP.h \
+ C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/NetworkBufferManagement.h \
+ C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/NetworkInterface.h \
+ C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/portable/NetworkInterface/include/phyHandling.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal.h \
+ ../Core/Inc/stm32f7xx_hal_conf.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_rcc.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_def.h \
+ ../Drivers/CMSIS/Device/ST/STM32F7xx/Include/stm32f7xx.h \
+ ../Drivers/CMSIS/Device/ST/STM32F7xx/Include/stm32f767xx.h \
+ ../Drivers/CMSIS/Include/core_cm7.h \
+ ../Drivers/CMSIS/Include/cmsis_version.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../Drivers/CMSIS/Include/mpu_armv7.h \
+ ../Drivers/CMSIS/Device/ST/STM32F7xx/Include/system_stm32f7xx.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_rcc_ex.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_exti.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_gpio.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_gpio_ex.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_dma.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_dma_ex.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_cortex.h \
+ C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/portable/NetworkInterface/include/stm32f7xx_hal_eth.h \
+ C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/portable/NetworkInterface/include/stm32fxx_hal_eth.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_def.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_flash.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_flash_ex.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_i2c.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_i2c_ex.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_pwr.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_pwr_ex.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_uart.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_uart_ex.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_pcd.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_ll_usb.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_pcd_ex.h \
+ C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/portable/NetworkInterface/include/stm32fxx_hal_eth.h
+
+../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h:
+
+../Core/Inc/FreeRTOSConfig.h:
+
+../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h:
+
+../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h:
+
+../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h:
+
+../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM7/r0p1/portmacro.h:
+
+../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h:
+
+../Middlewares/Third_Party/FreeRTOS/Source/include/task.h:
+
+../Middlewares/Third_Party/FreeRTOS/Source/include/list.h:
+
+../Middlewares/Third_Party/FreeRTOS/Source/include/queue.h:
+
+../Middlewares/Third_Party/FreeRTOS/Source/include/task.h:
+
+../Middlewares/Third_Party/FreeRTOS/Source/include/semphr.h:
+
+../Middlewares/Third_Party/FreeRTOS/Source/include/queue.h:
+
+C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/FreeRTOS_IP.h:
+
+../Core/Inc/FreeRTOSIPConfig.h:
+
+C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/FreeRTOSIPConfigDefaults.h:
+
+C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/FreeRTOS_errno_TCP.h:
+
+C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/IPTraceMacroDefaults.h:
+
+C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/portable/Compiler/GCC/pack_struct_start.h:
+
+C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/portable/Compiler/GCC/pack_struct_end.h:
+
+C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/FreeRTOS_Sockets.h:
+
+../Middlewares/Third_Party/FreeRTOS/Source/include/event_groups.h:
+
+../Middlewares/Third_Party/FreeRTOS/Source/include/timers.h:
+
+C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/FreeRTOS_IP_Private.h:
+
+C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/FreeRTOS_Sockets.h:
+
+C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/FreeRTOS_Stream_Buffer.h:
+
+C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/FreeRTOS_TCP_WIN.h:
+
+C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/FreeRTOS_TCP_IP.h:
+
+C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/FreeRTOS_DNS.h:
+
+C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/FreeRTOS_ARP.h:
+
+C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/NetworkBufferManagement.h:
+
+C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/include/NetworkInterface.h:
+
+C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/portable/NetworkInterface/include/phyHandling.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal.h:
+
+../Core/Inc/stm32f7xx_hal_conf.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_rcc.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_def.h:
+
+../Drivers/CMSIS/Device/ST/STM32F7xx/Include/stm32f7xx.h:
+
+../Drivers/CMSIS/Device/ST/STM32F7xx/Include/stm32f767xx.h:
+
+../Drivers/CMSIS/Include/core_cm7.h:
+
+../Drivers/CMSIS/Include/cmsis_version.h:
+
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+
+../Drivers/CMSIS/Include/cmsis_gcc.h:
+
+../Drivers/CMSIS/Include/mpu_armv7.h:
+
+../Drivers/CMSIS/Device/ST/STM32F7xx/Include/system_stm32f7xx.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_rcc_ex.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_exti.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_gpio.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_gpio_ex.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_dma.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_dma_ex.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_cortex.h:
+
+C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/portable/NetworkInterface/include/stm32f7xx_hal_eth.h:
+
+C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/portable/NetworkInterface/include/stm32fxx_hal_eth.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_def.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_flash.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_flash_ex.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_i2c.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_i2c_ex.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_pwr.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_pwr_ex.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_uart.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_uart_ex.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_pcd.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_ll_usb.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_pcd_ex.h:
+
+C:/Users/LENOVO/STM32CubeIDE/workspace_1.5.0/STM32_FREERTOS_STACK_TCP/Middlewares/Third_Party/Freertos_Plus/portable/NetworkInterface/include/stm32fxx_hal_eth.h:
